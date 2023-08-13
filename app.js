d8833a184fc41adb1f7563a7dd52fa341e78034b
Carlos Ezequiel Romero <ezequielromero3039@gmail.com>
2023-08-13 02:24:55 -0300

    Se configuró el servidor, el motor de plantillas ejs y el importaron las dependencias

---
+++ app.js
@@ -0,0 +1,42 @@
+// Imports
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const upload = require('express-fileupload');
+const path = require("path");
+require("dotenv").config();
+require("ejs");
+
+//Se conecta la BD
+
+const { conectarDB } = require("./database");
+
+conectarDB()
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middlewares
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(upload())
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+
+// Routes
+app.use(require("./routes/archivos.routes"));
+
+
+
+app.use((req, res, next) => {
+  return res.status(404).render("404");
+});
+
+// Starting the server
+app.listen(port, () => console.log(`The Server is running on http://localhost:${port}`));
