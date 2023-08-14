6b422596c704cf190538565e069e7ca5e1b96023
Carlos Ezequiel Romero <ezequielromero3039@gmail.com>
2023-08-14 15:26:04 -0300

    Se realizaron cambios

--- database.js
+++ database.js
@@ -1,32 +1,29 @@
 // Se importan las clases de la librería
-const { Sequelize, DataTypes } = require('sequelize');
+const { Sequelize, DataTypes } = require("sequelize");
 
 // Se crea una instancia de la conexión a la base de datos
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST, 
-        dialect: process.env.DB_DIALECT 
-    });
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  }
+);
 
-
-const conectarDB = async () =>{
-    try {
-        await sequelize.authenticate()
-        console.log("Base de Datos Conectada")
-    }catch (error) {
-        console.log("Error: " + error)
-    }
-}
-
-
+const conectarDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Base de Datos Conectada");
+  } catch (error) {
+    console.log("Error: " + error);
+  }
+};
 
 // Se exportan la conexión
 module.exports = {
-    sequelize,
-    DataTypes,
-    conectarDB
-    
-}
+  sequelize,
+  DataTypes,
+  conectarDB,
+};
